9f8c084900db1bfcd5463b274194a0a44f17aac6
root <root@DESKTOP-BSA9J6K>
2023-12-21 11:26:25 +0300

    created_date

--- todo/models.py
+++ todo/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.utils import timezone  # Import the timezone module
+from django.utils import timezone  
 
 class Todo(models.Model):
     text = models.CharField(max_length=100)
     complete = models.BooleanField(default=False)
-    created_at = models.DateTimeField(default=timezone.now)  # Add a new field for task creation time
+    created_at = models.DateTimeField(default=timezone.now) 
 
     def __str__(self):
         return self.text
